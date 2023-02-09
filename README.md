1e5f61ff85d1d47551b7c22fc1088145afb27efc
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-02-09 17:46:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# subject-and-obervable-angular-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular13-bootstrap-f9vuhx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  observableData:any;
+  observableData_1: any;
+  subjectData:any
+  constructor() {}
+
+  getObservableData(){
+    let myObservable= new Observable<any>(observer=>{
+      observer.next("Please go to office")
+    })
+    myObservable.subscribe((data)=>{
+      this.observableData= data
+    })
+
+
+    const source = of(1, 2, 3, 4, 5);
+    source.subscribe((data)=>{
+      this.observableData_1= data;
+    })
+  }
+
+  getSubjectData(){
+    let mySubject= new Subject();
+    mySubject.next("Please go to office");
+    mySubject.subscribe((data)=>{
+      this.subjectData= data;
+    })
+
+    mySubject.next([2,2,3,4,5]);
+    mySubject.next("Please go to office");
+    mySubject.next([2,2,3,4,5]);
+  }
+}
